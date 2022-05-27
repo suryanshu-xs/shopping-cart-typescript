a8c6a86dcc79f77d0609ad49d82823cbc30a4f06
Suryanshu <suryanshusweetu12345@gmail.com>
2022-05-27 06:39:33 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { useQuery } from 'react-query'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Components
+
+import Drawer from '@material-ui/core/Drawer'
+import LinearProgress from '@material-ui/core/LinearProgress'
+import Grid from '@material-ui/core/Grid'
+import AddShoppingCartOutlined from '@material-ui/icons/AddShoppingCartOutlined'
+import Badge from '@material-ui/core/Badge'
+
+// styles
+import { Wrapper } from './App.styles'
+
+
+export type CartItemType = {
+  id: number,
+  category: string,
+  description: string,
+  image: string,
+  price: number,
+  title: string,
+  amount: number;
 }
 
-export default App;
+
+const getProducts = async (): Promise<CartItemType[]> => {
+  return await (await fetch('https://fakestoreapi.com/products')).json()
+}
+
+
+const App = () => {
+
+  const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts)
+
+  console.log(data);
+
+  const getTotalItems = () => null
+
+  const handleAddToCart = () => null
+
+  const handleRemoveFromCart = () => null
+
+  if (isLoading) return <LinearProgress />
+  if (error) return <div> Something went wrong... </div>
+
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
---
+++ src/Items/items.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@material-ui/core";
+// Types
+import { CartItemType } from "../App";
+
+//Styles
+import { Wrapper } from "../App.styles";
+
+type Props = {
+    item:CartItemType;
+    // handleAddToCart:
+}
